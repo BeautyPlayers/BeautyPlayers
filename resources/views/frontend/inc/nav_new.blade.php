f5b0571bc20af234a74a975197a5f3a1944311fe
qasim-nadeem <qqnadeem@gmail.com>
2022-07-04 16:02:15 +0500

    fix header issues.

--- resources/views/frontend/inc/nav_new.blade.php
+++ resources/views/frontend/inc/nav_new.blade.php
@@ -1,3 +1,73 @@
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyDpgVwmJo0oG5ZfGKnkdiDCy75ELgptvC8&ver=3.exp"></script>
+
+<script>
+
+    // $(document).ready(function(){
+        function detect_current_location(param){
+            if(param.value == 'detect'){
+                navigator.geolocation.getCurrentPosition(
+                    function( position ){ // success cb
+                        console.log( position );
+                        var lat = position.coords.latitude;
+                        var lng = position.coords.longitude;
+                        var google_map_position = new google.maps.LatLng( lat, lng );
+                        var google_maps_geocoder = new google.maps.Geocoder();
+                        google_maps_geocoder.geocode(
+                            { 'latLng': google_map_position },
+                            function( results, status ) {
+                                var complete_address = results[0].formatted_address;
+                                
+                                console.log('results', results)
+                                get_chunks_from_address(results);
+                                var option = "<option value='"+complete_address+"'>"+complete_address+"</option>";
+                                console.log(option)
+                                $('.select_location').append(option);
+                                var last_option = $('.select_location option:last').val();
+                                console.log(last_option)
+                                $('.location-form input').val(last_option);
+                                $('.select_location').val(last_option);
+                            }
+                        );
+                    },
+                    function(){ // fail cb
+                        alert('failed to detect the current location')
+                    }
+                );        
+            }
+            
+        }
+        
+        
+    // })
+    
+    function get_chunks_from_address(addresses){
+        var country = "";
+        var city = "";
+        var address = addresses[0].formatted_address;
+        $("#addres_live").val(address)
+        $('#live_location').val(address)
+        addresses.forEach((val, index) => {
+            if(val.types.includes('country')){
+                country = val.formatted_address;                
+            }
+            if(val.types.includes('administrative_area_level_2')){
+                city = val.address_components[0].long_name;
+            }
+        })
+        
+        console.log('country', country);
+        console.log('city', city);
+        console.log('address', address);
+    }
+    
+    
+    $(document).ready(function(){
+        let detect = {value:"detect"};
+    detect_current_location(detect)
+})
+</script>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light"
         style="background-color: #fff !important;box-shadow: 1px 1px 6px #cbcbcb;; height: 100px !important;z-index:999;">
         <a class="navbar-brand mx-3" href="{{ route('home') }}">
@@ -24,12 +94,12 @@
                 <label class="font-weight-bold text-dark">Reseller</label>
             </span>
             <form class="form-inline my-2 my-lg-0 m-auto location-form">
-                <input class="form-control mr-sm-2 location-bar px-4" type="search"
+                <input class="form-control mr-sm-2 location-bar px-4" type="text"
                     placeholder="Tap on it to pick you location..." aria-label="Search">
             </form>
             <ul class="navbar-nav">
-                <li class="nav-item px-1">
-                    <a class="nav-link text-dark" href="{{ route('checkout.nearby_sellers') }}">
+                <li class="nav-item {{ Route::current()->getName() == 'services' ? 'pt-3' : 'px-1' }}">
+                    <a class="nav-link text-dark" href="https://template.doccure.io/html/template/search.html">
                         <img src="{{static_asset('assets/img/nearby.png')}}" alt="Beauty Players">
                         Nearby Experts
                     </a>

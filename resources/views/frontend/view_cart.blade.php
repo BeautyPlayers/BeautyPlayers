a9586baab786daac25110665e3dc5b6718a2d3f7
Nikul <nikulpatel1643@gmail.com>
2022-07-10 23:50:06 +0530

    Update according to the server

--- resources/views/frontend/view_cart.blade.php
+++ resources/views/frontend/view_cart.blade.php
@@ -144,11 +144,9 @@
                             <div class="col-md-6 text-center text-md-right">
                                 @if(Auth::check())
                                         @if(session()->has('fromShop'))
-                                            <li class="list-inline-item">
-                                                <a href="{{ route('checkout.shipping_info') }}" class="btn btn-primary btn-sm">
-                                                    {{translate('Continue to Shipping')}}
-                                                </a>
-                                            </li>
+                                            <a href="{{ route('checkout.shipping_info') }}" class="btn btn-primary btn-sm">
+                                                {{translate('Continue to Shipping')}}
+                                            </a>
                                         @else
                                             <a href="{{ route('checkout.nearby_sellers') }}" class="btn btn-primary fw-600">
                                                 {{ translate('Continue to Nearby Sellers ')}}
